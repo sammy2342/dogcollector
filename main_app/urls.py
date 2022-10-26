3aab47e94a608a375c3b41989458ec6b80109145
sammy2342 <sammyfares303@gmail.com>
2022-10-25 23:23:20 -0400

    complete add feeding add form and sorting

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,3 +1,4 @@
+from audioop import add
 from django.urls import path 
 from . import views
 
@@ -8,5 +9,6 @@ urlpatterns = [
     path('dogs/<int:dog_id>/', views.dogs_detail, name='detail'),
     path('dogs/create/', views.DogCreate.as_view(), name='dogs_create'),
     path('dogs/<int:pk>/update/', views.DogUpdate.as_view(), name='dogs_update'), 
-    path('dogs/<int:pk>/delete/', views.DogDelete.as_view(), name='dogs_delete')
+    path('dogs/<int:pk>/delete/', views.DogDelete.as_view(), name='dogs_delete'),
+    path('dogs/<int:dog_id>/add_feeding/', views.add_feeding, name='add_feeding'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import FeedingForm
 from .models import Dog
 from django.http import HttpResponse
 
@@ -20,7 +21,18 @@ def dogs_index(request):
 def dogs_detail(request, dog_id): 
   dog = Dog.objects.get(id=dog_id)
   # print(dog, 'this is the id for the dog')
-  return render(request, 'dogs/detail.html', { 'dog': dog })  
+  feeding_form = FeedingForm()
+  return render(request, 'dogs/detail.html', { 'dog': dog, 'feeding_form': feeding_form })  
+
+def add_feeding(request, dog_id): 
+  form = FeedingForm(request.POST)
+  # print(form, 'this is the form data')
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.dog_id = dog_id
+    # print(new_feeding.dog_id, 'thsisidsids')
+    new_feeding.save()
+    return redirect('detail', dog_id=dog_id)
 
 class DogCreate(CreateView): 
   model = Dog 
