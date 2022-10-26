fff71b455af5b28e1050dd3fd45392681453a411
sammy2342 <sammyfares303@gmail.com>
2022-10-25 22:12:16 -0400

    complete second model and add to detail page

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,16 @@
 from audioop import reverse
+from email.policy import default
+from random import choices
 from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+MEALS = (
+    ('B', 'Breakfast'), 
+    ('L', 'Lunch'), 
+    ('D', 'Dinner'),
+)
+
 class Dog(models.Model): 
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -16,3 +24,14 @@ class Dog(models.Model):
         # print(self.id, 'thos osasdfsadasdfsadf')
         return reverse('detail', kwargs={'dog_id': self.id})
 
+class Feeding(models.Model): 
+    date = models.DateField('Feeding Date')
+    meal = models.CharField(
+        max_length=1, 
+            choices=MEALS,
+            default=MEALS[0][0]
+    )
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+    def __str__(self): 
+        return f"{self.get_meal_display()} on {self.date}"
